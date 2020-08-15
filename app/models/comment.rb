4077db54d8fb205998be10962d22802383cf0623
masao <>
2020-08-15 22:51:42 +0900

    commentを追加し表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,17 +6,17 @@
 #  content    :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  board_id   :bigint           not null
 #  task_id    :bigint           not null
 #  user_id    :bigint           not null
 #
 # Indexes
 #
-#  index_comments_on_board_id  (board_id)
-#  index_comments_on_task_id   (task_id)
-#  index_comments_on_user_id   (user_id)
+#  index_comments_on_task_id  (task_id)
+#  index_comments_on_user_id  (user_id)
 #
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  validates :content, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -6,15 +6,13 @@
 #  content    :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  board_id   :bigint           not null
 #  task_id    :bigint           not null
 #  user_id    :bigint           not null
 #
 # Indexes
 #
-#  index_comments_on_board_id  (board_id)
-#  index_comments_on_task_id   (task_id)
-#  index_comments_on_user_id   (user_id)
+#  index_comments_on_task_id  (task_id)
+#  index_comments_on_user_id  (user_id)
 #
 require 'test_helper'
 
